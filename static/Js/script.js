a20c040399f1a7dcd843676fdd2481013c8ca308
augusto539 <61762968+augusto539@users.noreply.github.com>
2020-11-09 15:55:28 -0300

    Add files via upload

--- static/Js/script.js
+++ static/Js/script.js
@@ -1,40 +1,50 @@
 
-let palabras = ['hola', 'como', 'estas', 'yo','bien','y', 'vos','bien','tanbien','gracias','por','preguntar'];
-
+let palabras = ['de','la','que','el','en','los','se','del','un','por','con','no','ver','claro','veces','aquella','misma','programa','nueva','palabras','cual','internacional','fueron','van','una','mujer','esas','igual','tener','persona','dinero',
+                'embargo','iba','partido','personas','orden','grupo','cuenta','buena','pueden','quiere','tienen','frente','puesto','empresa','su','tras','para','cosas','es','fin','propia','al','ciudad','lo','libro','como','social','tema','guerra',
+                'manera','pero','sistema','sus','ellas','le','historia','total','ha','muchos','creo','me','Juan','tengo','Español','si','sin','cuatro','sobre','dentro','este','nuestro','condiciones','ya','punto','entre','dice','fuerza','cuando',
+                'hombre','solo','todo','cualquier','esta','noche','ser','amor','son','agua','dos','perece','puerta','haber','pesar','fue','zona','fuera','sabe','era','bajo','calle','muy','grandes','interior','años','nuestra','tampoco','hasta',
+                'ejemplo','desde','acuerdo','vista','mi','usted','campo','porque','estados','buen','hizo','hubiera','nadie','saber','han','obras','yo','horas','hay','posible','ex','vez','tarde','niños','puede','ley','presencia','todos','importante',];
 let Keystrokes = 0;
 let Correct_words = 0;
 let Wrong_words = 0;
+let numero_errores_palabla = 0;
 
 let palabras_2 = "";
 let palabras_3 = "";
 let cantidad_de_palabras = 0;
+let cantidad_de_palabras_2 = 0;
 let index;
 
 let string_palabras = "";
 let x_dividida = "";
 
-let numero_errores_palabla = 0;
+let time_1;
+let time_2;
+let time_3;
+let wpm;
 
 function escrivir_palabras(){
 
     string_palabras = "";
 
+    document.getElementById("errors_number").innerHTML = numero_errores_palabla;
+
     for (let i = 0; i < 20; i++) {
 
-        let Numero_1 = Math.round(Math.random() * (palabras.length - 0) + 0);
+        let Numero_1 = Math.round(Math.random() * ((palabras.length - 1) - 0) + 0);
 
 
         string_palabras += (palabras[Numero_1] + ' ');
+
+        document.getElementById(`${i}`).style.color = "#A7A7A7";
     }
 
     palabras_2 = string_palabras.split(' ');
 
-    for (let I = 0; I < 10; I++) {
+    for (let I = 0; I < 20; I++) {
         document.getElementById(`${I}`).innerHTML = palabras_2[I];
-        document.getElementById(`${I + 10}`).innerHTML = palabras_2[I + 10];
     }
 
-    console.log(palabras_2)
 }
 
 function input(){
@@ -43,24 +53,32 @@ function input(){
 
     Keystrokes += 1;
     
-    if (x.includes(' ')){
+    if (x.charAt(x.length-1) == " "){
 
         errores(x);
 
         cantidad_de_palabras += 1;
+        cantidad_de_palabras_2 += 1;
 
         if (cantidad_de_palabras == 20){
             escrivir_palabras()
+            cantidad_de_palabras = 0;
         }
-        if (cantidad_de_palabras > 20){
-            cantidad_de_palabras = 1;
+        if (cantidad_de_palabras_2 == 21){
+
+            Keystrokes = 0;
+            Correct_words = 0;
+            Wrong_words = 0;
+            numero_errores_palabla = 0;
+            cantidad_de_palabras_2 = 1;
+
+            document.getElementById("speed_number").innerHTML = "0 wpm";
         }
-
-        console.log(cantidad_de_palabras);
-
         x = " ";
-        document.getElementById("myInput").value = "";
+        document.getElementById("myInput").value = ".";
     }
+
+    tiempo();
     
     document.getElementById("Correct_words_number").innerHTML = Correct_words;
     document.getElementById("Wrong_words_number").innerHTML = Wrong_words;
@@ -69,47 +87,45 @@ function input(){
 
 function errores(x){
 
+    let errores_palabla = numero_errores_palabla;
 
-    if (string_palabras.includes(x)){
-        Correct_words += 1;
 
-        console.log(string_palabras);
+    for (let o = 1; o < (x.length)-1; o++) {
+
+        if  (palabras_2[cantidad_de_palabras].charAt(o-1) != x.charAt(o)){
+            numero_errores_palabla += 1;
+        }
+    }
+
+    document.getElementById("errors_number").innerHTML = numero_errores_palabla;
+
+    if (errores_palabla != numero_errores_palabla){
+        Wrong_words += 1;
+        document.getElementById(`${cantidad_de_palabras}`).style.color = "#c90301";
     }
     else{
-        Wrong_words += 1;
+        Correct_words += 1;
+        document.getElementById(`${cantidad_de_palabras}`).style.color = "#4CAF50";
     }
 
-    if (Wrong_words != 0){
-        if (Wrong_words == 1){
-
-            palabra_3 = palabras_2[Correct_words];
-
-            comparar_listas(palabra_3,x);
-    
-        }
-        else{
-            index = (Wrong_words + Correct_words) - 1; 
-
-            palabra_3 = palabras_2[index];
-
-            comparar_listas(palabra_3,x);
-        }
-    }
 }
 
-function comparar_listas(lista_1,lista_2){
-    for (let i = 0; i < lista_1.length; i++) {
-        if (lista_1[i-1] != lista_2[i-1]){
+function tiempo(){
 
-            console.log(lista_1);
-            console.log(lista_1[i-1]);
+    if (cantidad_de_palabras_2 == 1){
 
-            console.log(lista_2);
-            console.log(lista_2[i-1]);
+        time_1 = new Date();
 
-            numero_errores_palabla += 1;
-        } 
     }
-    document.getElementById("errors_number").innerHTML = numero_errores_palabla;
-}
+    if (cantidad_de_palabras_2 == 20){
 
+        time_2 = new Date();
+
+        time_3 = (time_2.getTime() - time_1.getTime()) / 1000;
+
+
+        wpm = (20/Math.abs(Math.round(time_3)))*60
+
+        document.getElementById("speed_number").innerHTML = wpm.toFixed(1) + " wpm";
+    }
+}
