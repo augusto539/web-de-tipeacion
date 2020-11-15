b1cadbac22489ff3a7e5914eadf6de919f3850ae
augusto539 <61762968+augusto539@users.noreply.github.com>
2020-11-14 23:46:16 -0300

    Add files via upload

--- form.py
+++ form.py
@@ -1,7 +1,13 @@
 from wtforms import Form, StringField, PasswordField
 from wtforms.fields.html5 import EmailField
+from wtforms import validators
 
 class forms(Form):
-    username = StringField()
-    pasword = PasswordField()
-    e_mail = EmailField()
+    username = StringField('username',
+                            [
+                                validators.Required(message='el usuario es requerido'),
+                                validators.length(min=4, max=25, message='prueva de color/ ingreda un usuario valido')
+                            ] 
+                            )
+    pasword = PasswordField('pasword')
+    e_mail = EmailField('e_mail')
--- main.py
+++ main.py
@@ -13,11 +13,24 @@ def index():
 @app.route('/logIn/', methods=['GET', 'POST'])
 def logIn():
     logIn_form = form.forms(request.form)
-    if request.method == 'POST':
+    if request.method == 'POST' and logIn_form.validate():
         print (logIn_form.username.data)
         print (logIn_form.pasword.data)
 
     return render_template('logIn.html', form = logIn_form)
+
+
+@app.route('/singIn/', methods=['GET', 'POST'])
+def singIn():
+    singIn_form = form.forms(request.form)
+    if request.method == 'POST':
+        print (singIn_form.username.data)
+        print (singIn_form.pasword.data)
+        print (singIn_form.e_mail.data)
+
+    return render_template('singIn.html', form = singIn_form)
+
+
 
 if __name__ == '__main__':
     app.run(debug = True, port= 8000)
