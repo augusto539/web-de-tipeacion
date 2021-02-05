a3a01f3d817b104b64ece22a91a028f7388f5e08
augusto <augustoantonelli88@gmail.com>
2021-02-04 21:48:56 -0300

    new table of database and little style changes

--- web_de_tipeacion/models.py
+++ web_de_tipeacion/models.py
@@ -6,6 +6,7 @@ class User(db.Model):
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     password = db.Column(db.String(20), nullable=False)
+    lenguage = db.Column(db.String(5))
     stats = db.relationship('Statistics', backref='user', lazy=True)
 
 
@@ -15,3 +16,12 @@ class Statistics(db.Model):
     errors = db.Column(db.Float, nullable=False)
     time = db.Column(db.DateTime, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+class Words(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.String, unique=True, nullable=False)
+    word_length = db.Column(db.Integer, nullable=False)
+    lenguage = db.Column(db.String(5), nullable=False)
+
+
