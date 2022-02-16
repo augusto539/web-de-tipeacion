28c84b02afe9f15c57577b7ba1aba6b05c288392
augusto <augustoantonelli88@gmail.com>
2022-02-16 16:11:24 -0300

    bug fix 3

--- src/index.js
+++ src/index.js
@@ -1,34 +1,25 @@
+//REQUIRES
 const express = require('express');
-const dotenv = require('dotenv')
-const cookieParser = require('cookie-parser')
 const path = require('path');
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser');
+const favicon = require('serve-favicon');
 //const os = require('os')
-
-
+// VARIABLES
 const app = express();
-
 // SETINGS
 app.set('port', process.env.PORT || 3000);
 //app.set('hostname', os.networkInterfaces()['Ethernet 2'][1]['address']); //get the ipv4 of the sistem
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'ejs');
-// parse application/x-www-form-urlencoded
+// MIDDLEWARES
+app.use(favicon('src/public/img/utn_logo.png'));
 app.use(bodyParser.urlencoded({ extended: true }));
-// parse application/json
 app.use(bodyParser.json());
-// cookies
 app.use(cookieParser());
-// variables de entorno
-dotenv.config({path: 'src/env/.env'});
-
-// ROUTES
 app.use(require('./routes/index'));
-
-// STATIC FILES
 app.use(express.static(path.join(__dirname, 'public')))
-
 // LISTENING THE SERVER
 //app.listen(app.get('port'),app.get('hostname') , () => console.log(`Server runing at http://${app.get('hostname')}:${app.get('port')}/`));
-app.listen(app.get('port'),app.get('hostname') , () => console.log(`Server runing on port ${app.get('port')}`));
+app.listen(app.get('port') , () => console.log(`Server runing on port: ${app.get('port')}`));
