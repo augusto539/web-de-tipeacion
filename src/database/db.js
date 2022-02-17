ae547b6318b4dbfebb577e0ddf959e057bfec519
augusto <augustoantonelli88@gmail.com>
2022-02-16 23:58:02 -0300

    second atempt of deploy in heroku

--- src/database/db.js
+++ src/database/db.js
@@ -2,13 +2,11 @@ const mysql = require('mysql');
 
 
 const connection = mysql.createConnection({
-    host: 'us-cdbr-east-05.cleardb.net',
-    user: 'bb991aa9ee14ec',
-    password: '11f993b4',
-    database: 'heroku_b0b210df1761f92'
+    host: process.env.DM_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DE_DATABASE
 });
-
-//mysql://bb991aa9ee14ec:11f993b4@us-cdbr-east-05.cleardb.net/heroku_b0b210df1761f92?reconnect=true
 
 connection.connect(error => {
     if (error) throw error;
